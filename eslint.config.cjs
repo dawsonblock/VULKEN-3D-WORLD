684993fdeafb3a397f8d9b12896f291af234ac32
dawsonblock <56236740+dawsonblock@users.noreply.github.com>
2025-08-16 15:42:05 -0600

    chore: rewrite ESLint config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -4,31 +4,9 @@ const globals = require('globals');
 module.exports = [
   {
     ignores: [
-
-      'assets/**',
-      'build_ci_sanity/**',
-      'cmake/**',
-      'docs/**',
-      'shaders/**',
-      'shaders_vk/**',
-      'tools/**',
-      'tests/**',
-      'src/**',
-      'apps/**',
-      'scripts/**'
-
-    'apps',
-      'assets',
-      'build_ci_sanity',
-      'cmake',
-      'docs',
-      'node_modules',
-      'scripts',
-      'shaders',
-      'shaders_vk',
-      'tests',
-      'tools'
-        main
+      'assets/**', 'build_ci_sanity/**', 'cmake/**',
+      'docs/**', 'shaders/**', 'shaders_vk/**',
+      'tools/**', 'tests/**'
     ]
   },
   js.configs.recommended,
@@ -36,10 +14,7 @@ module.exports = [
     languageOptions: {
       ecmaVersion: 2021,
       sourceType: 'script',
-      globals: {
-        ...globals.node,
-        ...globals.es2021
-      }
+      globals: { ...globals.node, ...globals.es2021 }
     },
     rules: {}
   }
