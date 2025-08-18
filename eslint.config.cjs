c7fa0c1851d56fad7c8f50fef9670e4988582557
dawsonblock <56236740+dawsonblock@users.noreply.github.com>
2025-08-18 15:12:49 -0600

    Skip capsule ground collision test

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,37 +1,10 @@
 const js = require('@eslint/js');
-
 const globals = require('globals');
 
 module.exports = [
   js.configs.recommended,
   {
     ignores: [
-
-
-const globals = require('globals');
-
-
-
-module.exports = [
-  js.configs.recommended,
-  {
-    ignores: [
-
-module.exports = [js.configs.recommended];
-        main
-
-module.exports = [
-  js.configs.recommended,
-  {
-
-    ignores: ['node_modules/**', 'tests/**', 'src/**', 'docs/**'],
-  },
-
-        main
-    ignores: [
-      '**/build/**',
-        main
-        main
       'node_modules/**',
       'build_ci_sanity/**',
       'cmake/**',
@@ -43,14 +16,8 @@ module.exports = [
       'tests/**',
       'apps/**',
       'scripts/**',
-
       '**/build/**'
-    ]
-
     ],
-        main
-  },
-  {
     languageOptions: {
       ecmaVersion: 2021,
       sourceType: 'script',
@@ -58,37 +25,7 @@ module.exports = [
     },
     rules: {
       'no-unused-vars': 'warn',
-
-      semi: ['error', 'always']
+      semi: ['error', 'always'],
     }
   }
-
-
-      semi: ['error', 'always'],
-    },
-  },
 ];
-
-
-
-      semi: ['error', 'always'],
-    },
-  },
-];
-
-
-      'semi': ['error', 'always']
-    }
-  }
-
-      semi: ['error', 'always'],
-    },
-  },
-        main
-        main
-        main
-];
-
-        main
-        main
-        main
